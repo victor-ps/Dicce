32b1c8157e9e21e6213f24632a8a0f5a83b1341b
Victor Andre de Paula e Silva <victorandre.ps@gmail.com>
2022-04-27 18:30:31 +1000

    Initial Commit

---
+++ Dicce/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dicce
+//
+//  Created by Victor Andre de Paula e Silva on 27/4/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
